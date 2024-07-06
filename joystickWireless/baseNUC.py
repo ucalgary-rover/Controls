a03f7965a4e3f52a7e3b37f3505431a8d7445ae8
Sajan21 <89943300+Sajan21@users.noreply.github.com>
2024-07-06 11:49:27 -0600

    Update baseNUC.py
    server no longer dies/timesout after 60seconds

--- joystickWireless/baseNUC.py
+++ joystickWireless/baseNUC.py
@@ -12,64 +12,72 @@ pygame.joystick.init()
 joystick_count = pygame.joystick.get_count()
 print("Number of joysticks: ", joystick_count)
 joystick0 = pygame.joystick.Joystick(0)
-#joystick1 = pygame.joystick.Joystick(1)
+joystick1 = pygame.joystick.Joystick(1)
 joystick0.init()
-#joystick1.init()
+joystick1.init()
 
 print("BASE: Initialized joystick ", joystick0.get_name())
-#print("BASE: Initialized joystick ", joystick1.get_name())
+print("BASE: Initialized joystick ", joystick1.get_name())
 
 async def send_commands():
-    async with websockets.connect(uri) as websocket:
-        while True:
-            #event processing step.
-            for event in pygame.event.get():
-                #to exit
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    exit()
+	async with websockets.connect(uri) as websocket:
+		try:
+			while True:
+				#event processing step.
+				for event in pygame.event.get():
+		            #to exit
+					if event.type == pygame.QUIT:
+						pygame.quit()
+						exit()
 
-                #button on the joystick is pressed down
-                if event.type == pygame.JOYBUTTONDOWN:
-                    #what gets sent to the rover nuc
-                    command = f"button_down:{event.button}"
-                    await websocket.send(command)
-                    print("BASE: Button Pressed:", event.button)
+		            #button on the joystick is pressed down
+					if event.type == pygame.JOYBUTTONDOWN:
+						#what gets sent to the rover nuc
+						command = f"button_down:{event.button}"
+						await websocket.send(command)
+						print("BASE: Button Pressed:", event.button)
 
-                    if event.button == 2:
-                        print("BASE: off")
+						if event.button == 2:
+							print("BASE: off")
 
-                    elif event.button == 3:
-                        print("BASE: Increase driver speed")
+						elif event.button == 3:
+							print("BASE: Increase driver speed")
 
-                    elif event.button == 4:
-                        print("BASE: Decrease driver speed")
+						elif event.button == 4:
+							print("BASE: Decrease driver speed")
 
-                    elif event.button == 6:
-                        print("BASE: Drive mode")
+						elif event.button == 6:
+							print("BASE: Drive mode")
 
-                    elif event.button == 8:
-                        print("BASE: Previous Joint Movement mode")
+						elif event.button == 8:
+							print("BASE: Previous Joint Movement mode")
 
-                    elif event.button == 9:
-                        print("BASE: Next Joint Movement mode")
-                
-                # button on the joystick is pressed up
-                if event.type == pygame.JOYBUTTONUP:
-                    command = f"button_up:{event.button}"
-                    await websocket.send(command)
-                    if event.button == 2:
-                        print("BASE: off")
-                        exit()
+						elif event.button == 9:
+							print("BASE: Next Joint Movement mode")
+		            
+		            # button on the joystick is pressed up
+					if event.type == pygame.JOYBUTTONUP:
+						command = f"button_up:{event.button}"
+						await websocket.send(command)
+						if event.button == 2:
+							print("BASE: off")
+							exit()
 
-                if event.type == pygame.JOYAXISMOTION:
-                    command = f"axis_motion:{event.axis}:{event.value}"
-                    await websocket.send(command)
-                    print("BASE: Axis Motion:", event.axis, event.value)
+					if event.type == pygame.JOYAXISMOTION:
+						command = f"axis_motion:{event.axis}:{event.value}"
+						await websocket.send(command)
+						print("BASE: Axis Motion:", event.axis, event.value)
 
-                if event.type == pygame.JOYHATMOTION:
-                    command = f"hat_motion:{event.hat}:{event.value}"
-                    await websocket.send(command)
-                    print("BASE: Hat Motion:", event.hat, event.value)
+					if event.type == pygame.JOYHATMOTION:
+						command = f"hat_motion:{event.hat}:{event.value}"
+						await websocket.send(command)
+						print("BASE: Hat Motion:", event.hat, event.value)
+		                
+		except (websockets.ConnectionClosedError, websockets.InvalidURI, websockets.InvalidHandshake) as e:
+			print(f"exception error: {e}")
+			await asyncio.sleep(5)
+		except Exception as e:
+			print(f"unexpected error: {e}")
+			await asyncio.sleep(5)
 
 asyncio.get_event_loop().run_until_complete(send_commands())
