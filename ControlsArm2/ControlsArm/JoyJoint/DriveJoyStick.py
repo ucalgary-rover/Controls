39c8bca36cfd6c689772e4e0f2b5724442d45296
Marc Adriel Ty <88467549+MarcTy@users.noreply.github.com>
2024-08-11 17:45:03 -0600

    Update DriveJoyStick.py

--- ControlsArm2/ControlsArm/JoyJoint/DriveJoyStick.py
+++ ControlsArm2/ControlsArm/JoyJoint/DriveJoyStick.py
@@ -13,64 +13,103 @@ import time
 VHubSerial_motors = 697178
 VHubSerial_servo = 697066 #verify this matches what is in the rover
 
-leftSideDriveMotors = DCMotor()
-rightSideDriveMotors = DCMotor()
+leftSideFrontDriveMotors = DCMotor()
+rightSideFrontDriveMotors = DCMotor()
+
+leftSideBackDriveMotors = DCMotor()
+rightSideBackDriveMotors = DCMotor()
 
 def driver_init():
-    leftSideDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
-    rightSideDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
+    leftSideFrontDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
+    rightSideFrontDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
+    leftSideBackDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
+    rightSideBackDriveMotors.setDeviceSerialNumber(VHubSerial_motors)
 
-    leftSideDriveMotors.setHubPort(0)
-    rightSideDriveMotors.setHubPort(5)
+    leftSideFrontDriveMotors.setHubPort(0)
+    rightSideFrontDriveMotors.setHubPort(5)
+    # double check following hub ports
+    leftSideBackDriveMotors.setHubPort(1)
+    rightSideBackDriveMotors.setHubPort(4)
     
     try:
-        leftSideDriveMotors.openWaitForAttachment(5000)
-        rightSideDriveMotors.openWaitForAttachment(5000)
+        leftSideFrontDriveMotors.openWaitForAttachment(5000)
+        rightSideFrontDriveMotors.openWaitForAttachment(5000)
+        leftSideBackDriveMotors.openWaitForAttachment(5000)
+        rightSideBackDriveMotors.openWaitForAttachment(5000)
         print("Test connection")
         print("Drivers connected")
-        print("is left drive attached: ", leftSideDriveMotors.getAttached())
+        print("is left front drive attached: ", leftSideFrontDriveMotors.getAttached())
+        print("is right front drive attached: ", rightSideFrontDriveMotors.getAttached())
+        print("is left back drive attached ", leftSideBackDriveMotors.getAttached())
+        print("is right back drive attached: ", rightSideBackDriveMotors.getAttached())
     except:
         print("Driver Failed to connect")
         print("Drivers not connected")
         
     else:
-        leftSideDriveMotors.setCurrentLimit(10)
-        leftSideDriveMotors.setTargetVelocity(0)
-        leftSideDriveMotors.setAcceleration(5)
-        #leftSideDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
+        leftSideFrontDriveMotors.setCurrentLimit(10)
+        leftSideFrontDriveMotors.setTargetVelocity(0)
+        leftSideFrontDriveMotors.setAcceleration(5)
+        #leftSideFrontDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
+        rightSideFrontDriveMotors.setCurrentLimit(10)
+        rightSideFrontDriveMotors.setTargetVelocity(0)
+        rightSideFrontDriveMotors.setAcceleration(5)
+        #rightSideFrontDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
+        
+        leftSideBackDriveMotors.setCurrentLimit(10)
+        leftSideBackDriveMotors.setTargetVelocity(0)
+        leftSideBackDriveMotors.setAcceleration(5)
+        #leftSideBackDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
+        rightSideBackDriveMotors.setCurrentLimit(10)
+        rightSideBackDriveMotors.setTargetVelocity(0)
+        rightSideBackDriveMotors.setAcceleration(5)
+        #rightSideBackDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
 
-        rightSideDriveMotors.setCurrentLimit(10)
-        rightSideDriveMotors.setTargetVelocity(0)
-        rightSideDriveMotors.setAcceleration(5)
-        #rightSideDriveMotors.setFanMode(DCMotorFanMode.DCMOTOR_FAN_MODE_OFF)
-
-        #leftSideDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
-        #rightSideDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
-        leftSideDriveMotors.setAcceleration(100)
-        rightSideDriveMotors.setAcceleration(100)
-        # leftSideDriveMotors.setVelocityLimit(100)
-        # rightSideDriveMotors.setVelocityLimit(100)
-        leftSideDriveMotors.setTargetVelocity(0)
-        rightSideDriveMotors.setTargetVelocity(0)
-        leftSideDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
-        rightSideDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
+        #leftSideFrontDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
+        #rightSideFrontDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
+        leftSideFrontDriveMotors.setAcceleration(100)
+        rightSideFrontDriveMotors.setAcceleration(100)
+        #leftSideBackDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
+        #rightSideBackDriveMotors.setControlMode(DCMotorControlMode.DCMOTOR_CONTROL_MODE_SPEED)
+        leftSideBackDriveMotors.setAcceleration(100)
+        rightSideBackDriveMotors.setAcceleration(100)
+        
+        # leftSideFrontDriveMotors.setVelocityLimit(100)
+        # rightSideFrontDriveMotors.setVelocityLimit(100)
+        # leftSideBackDriveMotors.setVelocityLimit(100)
+        # rightSideBackDriveMotors.setVelocityLimit(100)
+        
+        leftSideFrontDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
+        rightSideFrontDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
+        leftSideBackDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
+        rightSideBackDriveMotors.setFanMode(0x2)  # 0x2 is FAN_MODE_ON
 
 def drive_forward(speed):
-    leftSideDriveMotors.setTargetVelocity(speed)
-    rightSideDriveMotors.setTargetVelocity(-speed)
+    leftSideFrontDriveMotors.setTargetVelocity(speed)
+    rightSideFrontDriveMotors.setTargetVelocity(-speed)
+    leftSideBackDriveMotors.setTargetVelocity(speed)
+    rightSideBackDriveMotors.setTargetVelocity(-speed)
 
 def drive_backward(speed):
-    leftSideDriveMotors.setTargetVelocity(-speed)
-    rightSideDriveMotors.setTargetVelocity(speed)
+    leftSideFrontDriveMotors.setTargetVelocity(-speed)
+    rightSideFrontDriveMotors.setTargetVelocity(speed)
+    leftSideBackDriveMotors.setTargetVelocity(-speed)
+    rightSideBackDriveMotors.setTargetVelocity(speed)
 
 def drive_right(speed):
-    leftSideDriveMotors.setTargetVelocity(speed)
-    rightSideDriveMotors.setTargetVelocity(speed)
+    leftSideFrontDriveMotors.setTargetVelocity(speed)
+    rightSideFrontDriveMotors.setTargetVelocity(speed)
+    leftSideBackDriveMotors.setTargetVelocity(speed)
+    rightSideBackDriveMotors.setTargetVelocity(speed)
 
 def drive_left(speed):
-    leftSideDriveMotors.setTargetVelocity(-speed)
-    rightSideDriveMotors.setTargetVelocity(-speed)
+    leftSideFrontDriveMotors.setTargetVelocity(-speed)
+    rightSideFrontDriveMotors.setTargetVelocity(-speed)
+    leftSideBackDriveMotors.setTargetVelocity(-speed)
+    rightSideBackDriveMotors.setTargetVelocity(-speed)
 
 def drive_stop():
-    leftSideDriveMotors.setTargetVelocity(0)
-    rightSideDriveMotors.setTargetVelocity(0)
+    leftSideFrontDriveMotors.setTargetVelocity(0)
+    rightSideFrontDriveMotors.setTargetVelocity(0)
+    leftSideBackDriveMotors.setTargetVelocity(0)
+    rightSideBackDriveMotors.setTargetVelocity(0)
