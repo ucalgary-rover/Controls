9fad81e5893605f1b565a4c5860f89506c34119f
john-sperida <j.esperida49@gmail.com>
2025-02-07 15:51:51 -0700

    Update Controller.h to fit changes in Controller.cpp

--- prj/Base/Controller.h
+++ prj/Base/Controller.h
@@ -6,25 +6,27 @@
 #include <SDL2/SDL.h>
 #include <iostream>
 
-typedef void (*ControllerFunc)(void* arg);
+// Used to streamline the struct
+typedef void (*ControllerFunc)(void *args);
+
 // a struct for function pointers used for button mapping
 struct buttonFunctions {
-    ControllerFunc Button;
-    void (*BUTTON_A)();
-    void (*BUTTON_B)();
-    void (*BUTTON_X)();
-    void (*BUTTON_Y)();
-    void (*BUTTON_BACK)();
-    void (*BUTTON_GUIDE)();
-    void (*BUTTON_START)();
-    void (*BUTTON_LEFTSTICK)();
-    void (*BUTTON_RIGHTSTICK)();
-    void (*BUTTON_LEFTSHOULDER)();
-    void (*BUTTON_RIGHTSHOULDER)();
-    void (*BUTTON_DPAD_UP)();
-    void (*BUTTON_DPAD_DOWN)();
-    void (*BUTTON_DPAD_LEFT)();
-    void (*BUTTON_DPAD_RIGHT)();
+  ControllerFunc Button;
+  ControllerFunc BUTTON_A;
+  ControllerFunc BUTTON_B;
+  ControllerFunc BUTTON_X;
+  ControllerFunc BUTTON_Y;
+  ControllerFunc BUTTON_BACK;
+  ControllerFunc BUTTON_GUIDE;
+  ControllerFunc BUTTON_START;
+  ControllerFunc BUTTON_LEFTSTICK;
+  ControllerFunc BUTTON_RIGHTSTICK;
+  ControllerFunc BUTTON_LEFTSHOULDER;
+  ControllerFunc BUTTON_RIGHTSHOULDER;
+  ControllerFunc BUTTON_DPAD_UP;
+  ControllerFunc BUTTON_DPAD_DOWN;
+  ControllerFunc BUTTON_DPAD_LEFT;
+  ControllerFunc BUTTON_DPAD_RIGHT;
 };
 
 // A class for initializing controllers and running the pollEvent loop
@@ -32,112 +34,140 @@ struct buttonFunctions {
 // in the list <<<<<<<<<<<<<<<<<<<<<<<
 class ControllerHolder {
 
-    // class for joysticks (they are generic and can be applied to any
-    // controller)
-    class Stick {
-    public:
-        // identifiers for the axes
-        // funny enough, triggers also operate with axes
-        SDL_GameControllerAxis X_AXIS;
-        SDL_GameControllerAxis Y_AXIS;
+  // class for joysticks (they are generic and can be applied to any controller)
+  class Stick {
+  private:
+    // identifiers for the axes
+    // funny enough, triggers also operate with axes
+    SDL_GameControllerAxis X_AXIS;
+    SDL_GameControllerAxis Y_AXIS;
 
-        // positions values from -32768 - 32767 (note up and left are negative)
-        float m_xPos;
-        float m_yPos;
+    // positions values from -32768 - 32767 (note up and left are negative)
+    float m_xPos;
+    float m_yPos;
 
-        // raw values for the use of moving deadzone comparison
-        float m_rawX;
-        float m_rawY;
+    // raw values for the use of moving deadzone comparison
+    float m_rawX;
+    float m_rawY;
 
-    public:
-        /**
-         *Assigns a joystick's axes (ex. right stick has a rightX and a rightY
-         *axis)
-         *
-         *@param values The pointer to the game controller object
-         *
-         *@return None
-         *
-         */
-        Stick(SDL_GameControllerAxis xID, SDL_GameControllerAxis yID) {
-            X_AXIS = xID;
-            Y_AXIS = yID;
-        }
+  public:
+    /**
+     *Assigns a joystick's axes (ex. right stick has a rightX and a rightY axis)
+     *
+     *@param values The pointer to the game controller object
+     *
+     *@return None
+     *
+     */
+    Stick(SDL_GameControllerAxis xID, SDL_GameControllerAxis yID) {
+      X_AXIS = xID;
+      Y_AXIS = yID;
+    }
+  };
+
+  // class for controllers
+  class Controller {
+
+  private:
+    // the identifier (a pointer to the controller object and its instance ID)
+    SDL_GameController *m_pointerID;
+    SDL_JoystickID m_instanceID;
+
+    // the values used to initialize sticks just identify a generic left or
+    // right joystick
+    Stick m_leftStick;
+    Stick m_rightStick;
+
+    // struct for button functions
+    buttonFunctions m_buttonFuncs;
+
+  public:
+    // getter/setter funcs
+
+    // these are never changed unless there is a new controller anyways so no
+    // setter needed
+    SDL_GameController *getPointerID() { return m_pointerID; };
+    SDL_JoystickID getInstanceID() { return m_instanceID; };
+
+    Stick getLeftStick() { return m_leftStick; };
+    Stick getRightStick() { return m_rightStick; };
+    void setLeftStick(Stick lStick) { m_leftStick = lStick; };
+    void setRightStick(Stick rStick) { m_rightStick = rStick; };
+
+    void setButtonFuncs(buttonFunctions funcsStruct) {
+      m_buttonFuncs = funcsStruct;
     };
 
-    // class for controllers
-    class Controller {
+    // A constructor for making a completely blank controller object
+    Controller() {
+      m_pointerID;
 
-    public:
-        // the identifier (a pointer to the controller object and its instance
-        // ID)
-        SDL_GameController* m_pointerID;
-        SDL_JoystickID m_instanceID;
+      // invalid joystick ID
+      m_instanceID;
+    }
 
-        // the values used to initialize sticks just identify a generic left or
-        // right joystick
-        Stick m_leftStick;
-        Stick m_rightStick;
+    /**
+     *Assigns the controller's identifier once it is initialized
+     *
+     *@param values The pointer to the game controller object
+     *
+     *@return None
+     *
+     */
+    Controller(SDL_GameController *identifier) {
+      m_pointerID = identifier;
 
-        // getter funcs
-        SDL_GameController* getPointerID() { return m_pointerID; };
-        SDL_JoystickID getInstanceID() { return m_instanceID; };
+      // joystick / controller instance ID <- joystick pointer <- controller
+      // pointer
+      m_instanceID;
+    }
 
-        // A constructor for making a completely blank controller object
-        Controller() {
-            m_pointerID;
+    /**
+     *Executes a function when a button is pressed
+     *
+     *@param buttonID The ID of the button pressed
+     *
+     *@return None
+     *
+     */
+    void buttonResponse(Uint8 buttonID);
+  };
 
-            // invalid joystick ID
-            m_instanceID;
-        }
-
-        /**
-         *Assigns the controller's identifier once it is initialized
-         *
-         *@param values The pointer to the game controller object
-         *
-         *@return None
-         *
-         */
-        Controller(SDL_GameController* identifier) {
-            m_pointerID = identifier;
-
-            // joystick / controller instance ID <- joystick pointer <-
-            // controller pointer
-            m_instanceID;
-        }
-    };
+private:
+  // Holding the controller objects
+  Controller m_controllerList[2];
 
 public:
-    // Holding the controller objects
-    Controller m_controllerList[2];
+  // getter and setter function
+  Controller getControllerFromList(int i) {
+    return (i == 1 || i == 0) ? m_controllerList[i] : m_controllerList[0];
+  };
 
-    // Holding the structs to map to each controller
-    buttonFunctions m_buttonList[2];
+  void setControllerFromList(int i, Controller controller) {
+    i = (i == 1 || i == 0) ? i : 0;
+    m_controllerList[i] = controller;
+  };
+  /**
+   *Initializes SDL and maps the appropriate functions to controllers
+   *
+   *@param values The structs with mappings for each controller, controllers 1
+   *and 2 respectively
+   *
+   *@return None
+   *
+   */
+  ControllerHolder(buttonFunctions functionStruct1,
+                   buttonFunctions functionStruct2);
 
-    /**
-     *Initializes controllers and mapps the appropriate buttons to them
-     *
-     *@param values The structs with mappings for each controller, controllers 1
-     *and 2 respectively
-     *
-     *@return None
-     *
-     */
-    ControllerHolder(buttonFunctions functionStruct1,
-                     buttonFunctions functionStruct2);
-
-    /**
-     *Runs the event loop to recieve any controller events
-     *
-     *@param values None
-     *
-     *@return None
-     *
-     */
-    void eventLoop();
+  /**
+   *Runs the event loop to recieve any controller events
+   *
+   *@param values None
+   *
+   *@return None
+   *
+   */
+  void eventLoop();
 };
 
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-#endif
