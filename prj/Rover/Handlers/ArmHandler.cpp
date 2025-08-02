1414fec44712fd9eddae7da0814f53e30ec3b266
john-sperida <j.esperida49@gmail.com>
2025-08-02 15:39:42 -0600

    Added stepper calibration and position json to ArmHandler
    - As described in the Title
    - A little rudimentary
    
    Created by:  Johan Esperida
    From team:  Controls Software

--- prj/Rover/Handlers/ArmHandler.cpp
+++ prj/Rover/Handlers/ArmHandler.cpp
@@ -1,13 +1,29 @@
 #include "ArmHandler.h"
-
 #include "ArmModel.h"
 
 // Constructor
 ArmHandler::ArmHandler(Arm& arm, MessageQueue& armQueue) :
     m_arm(arm), m_armQueue(armQueue) {
 
+    // Open the file to store stepper positions
+    std::ifstream positionFile(ARM_LAST_KNOWN_POS_FILE, std::ifstream::binary);
+    if (!positionFile.is_open()) {
+        Logging::logE(file, "Failed to open angle storage file.");
+    }
+    Json::CharReaderBuilder readerBuilder;
+    std::string errs;
+    Json::parseFromStream(readerBuilder, positionFile, &m_lastKnownPos, &errs);
+    angleFile.close();
+    // Updates for this not set up yet <------------------------------------
+
     // Initialize static Arm Model Wrapper
     ArmModel::initialize();
+
+    for (int i = 0; i < m_arm->getDOF(); i++) {
+        if (motorTypes[motor] == MOTOR_TYPE_STEPPER_MOTOR) {
+            calibrateStepper(i);
+        }
+    }
 }
 
 void ArmHandler::handleManualArmMessage(ArmManualMessage message) {
@@ -76,6 +92,56 @@ void ArmHandler::updateMotorAngles(std::array<double, T> new_angles) {
         m_arm.getArmMotorHandle(handler, i);
         updateMotorAngle(handler, new_angles.at(i));
     }
+}
+
+void ArmHandler::calibrateStepper(int stepper) {
+    // This function should be called before the arm is used to ensure
+    // the steppers are at the correct orientation
+
+    // 1 for clockwise, -1 for counterclockwise
+    int direction = -1;
+    MotorHandlerReturn motorStuct;
+    MotorHandlerReturn encoderStuct;
+    int currentIndex; // will change to its actual value when the stepper finds
+                      // it
+    int initialIndex; // index given by initial index call
+    int change = -10; // starting off negative so first sweep is clockwise
+
+    // get the encoder and stepper
+    m_arm->getArmMotorHandle(&motorStuct, wheel);
+    m_arm->getArmEncoderHandle(&encoderStuct, stepper);
+
+    // Get the initial value of the index
+    initialIndex = getIndexPosition(*encoderStuct.handler.encoder);
+
+    // Get the current position of the wheel
+    int lastKnownAngle = m_lastKnownPos[driveIndexToString(wheel)].asInt();
+    PhidgetStepper_addPositionOffset(*motorStuct.handler.stepperMotor,
+                                     -lastKnownAngle);
+
+    // start sweeping until index changes
+    while (currentIndex == initialIndex) {
+        angleGoal += change * direction;
+
+        PhidgetStepper_setTargetPosition(*motorStuct.handler.stepperMotor,
+                                         angleGoal);
+
+        // Get the current value of the index
+        currentIndex = getIndexPosition(*encoderStuct.handler.encoder);
+
+        if (abs(angleGoal) > 100) {
+            Logging::logE(file,
+                          "Calibration failed: angle goal exceeded limit.");
+            exit(1);
+        }
+    }
+
+    // Reset the position of the stepper motor to 0 where the index is
+    PhidgetStepper_setTargetPosition(*motorStuct.handler.stepperMotor,
+                                     currentIndex);
+    PhidgetStepper_addPositionOffset(*motorStuct.handler.stepperMotor,
+                                     -currentIndex);
+    PhidgetEncoder_setPosition(*encoderStuct.handler.encoder, 0);
 }
 
 // Destructor
--- prj/Rover/Handlers/ArmHandler.h
+++ prj/Rover/Handlers/ArmHandler.h
@@ -15,11 +15,19 @@ public:
 
     void start() override;
 
+    /**
+     * @brief Calibrates steppers so they start in the zero positions
+     * @param stepper the index of the stepper to be calibrated
+     */
+    void calibrateStepper(int stepper);
+
 private:
     Arm& m_arm;
 
     // Arm Queue
     MessageQueue& m_armQueue; // References the armQueue in Rover.cpp
+
+    Json::Value m_lastKnownPos; // Last known position of the arm
 
     // Threads
     std::thread m_armQueueThread;
